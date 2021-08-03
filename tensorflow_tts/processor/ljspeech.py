4420974a394af2fc6c81e2b35e12fe0e900b3734
Grigorev Grigoriy <ggrigorev@croc.ru>
2021-08-03 23:02:52 +0400

    prepare ljspeech processor for udmurt lang

--- tensorflow_tts/processor/ljspeech.py
+++ tensorflow_tts/processor/ljspeech.py
@@ -24,105 +24,15 @@ from tensorflow_tts.processor import BaseProcessor
 from tensorflow_tts.utils import cleaners
 from tensorflow_tts.utils.utils import PROCESSOR_FILE_NAME
 
-valid_symbols = [
-    "AA",
-    "AA0",
-    "AA1",
-    "AA2",
-    "AE",
-    "AE0",
-    "AE1",
-    "AE2",
-    "AH",
-    "AH0",
-    "AH1",
-    "AH2",
-    "AO",
-    "AO0",
-    "AO1",
-    "AO2",
-    "AW",
-    "AW0",
-    "AW1",
-    "AW2",
-    "AY",
-    "AY0",
-    "AY1",
-    "AY2",
-    "B",
-    "CH",
-    "D",
-    "DH",
-    "EH",
-    "EH0",
-    "EH1",
-    "EH2",
-    "ER",
-    "ER0",
-    "ER1",
-    "ER2",
-    "EY",
-    "EY0",
-    "EY1",
-    "EY2",
-    "F",
-    "G",
-    "HH",
-    "IH",
-    "IH0",
-    "IH1",
-    "IH2",
-    "IY",
-    "IY0",
-    "IY1",
-    "IY2",
-    "JH",
-    "K",
-    "L",
-    "M",
-    "N",
-    "NG",
-    "OW",
-    "OW0",
-    "OW1",
-    "OW2",
-    "OY",
-    "OY0",
-    "OY1",
-    "OY2",
-    "P",
-    "R",
-    "S",
-    "SH",
-    "T",
-    "TH",
-    "UH",
-    "UH0",
-    "UH1",
-    "UH2",
-    "UW",
-    "UW0",
-    "UW1",
-    "UW2",
-    "V",
-    "W",
-    "Y",
-    "Z",
-    "ZH",
-]
-
 _pad = "pad"
 _eos = "eos"
 _punctuation = "!'(),.:;? "
 _special = "-"
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ["@" + s for s in valid_symbols]
+_letters = "АБВГДЕЁЖӜЗӞИӤЙКЛМНОӦПРСТУФХЦЧӴШЩЪЫЬЭЮЯабвгдеёжӝзӟиӥйклмноӧпрстуфхцчӵшщъыьэюя"
 
 # Export all symbols:
 LJSPEECH_SYMBOLS = (
-    [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet + [_eos]
+    [_pad] + list(_special) + list(_punctuation) + list(_letters) + [_eos]
 )
 
 # Regular expression matching text enclosed in curly braces:
@@ -133,7 +43,7 @@ _curly_re = re.compile(r"(.*?)\{(.+?)\}(.*)")
 class LJSpeechProcessor(BaseProcessor):
     """LJSpeech processor."""
 
-    cleaner_names: str = "english_cleaners"
+    cleaner_names: str = "basic_cleaners"
     positions = {
         "wave_file": 0,
         "text": 1,
